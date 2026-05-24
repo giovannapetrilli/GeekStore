f37706cdaf1a43821741565b7b7d243ac3feb377
Giovanna Petrilli <giovanna11petrilli@gmail.com>
2026-05-24 15:30:45 -0300

    Executando FastAPI em paralelo para permitir testes de contrato da API.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,12 @@
 import os
+import sys
 import pytest
 from fastapi.testclient import TestClient
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 from main import app, init_db
+
 
 @pytest.fixture
 def client(monkeypatch):
